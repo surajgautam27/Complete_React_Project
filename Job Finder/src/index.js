504954f7bb9c0256816d320dfda42e0569636e5c
Suraj <surajgautam1111@gmai;l.com>
2023-10-26 14:32:22 +0545

    redux toolkit slice

--- Job Finder/src/index.js
+++ Job Finder/src/index.js
@@ -3,10 +3,17 @@ import {createRoot} from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
 import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App tab = 'home'/>)
+root.render(
+<Provider store={store}>
+<App tab = 'home'/>
+
+</Provider>
+)
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import 'normalize.css'
