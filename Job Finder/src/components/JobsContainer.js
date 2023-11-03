626d26b7288a7c9566a41eb1f2129b0f0f149926
Suraj <surajgautam1111@gmai;l.com>
2023-11-03 15:55:12 +0545

    jobs page complete

--- Job Finder/src/components/JobsContainer.js
+++ Job Finder/src/components/JobsContainer.js
@@ -1,11 +1,45 @@
-import React from 'react'
-
+import { useDispatch, useSelector } from "react-redux"
+import Wrapper from "../assets/wrappers/JobsContainer"
+import Job from "./Job"
+import { useEffect } from "react"
+import { getAllJobs } from "../features/allJobs/allJobsSlice"
 const JobsContainer = () => {
-  return (
-    <div>
-      Jobs Container
-    </div>
-  )
+    const {jobs , isLoading} = useSelector((store)=>store.allJobs)
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getAllJobs())
+     console.log('wee')
+    },[getAllJobs])
+    if(isLoading)
+    {
+        return(
+            <Wrapper>
+                <h2> Loading ....</h2>
+            </Wrapper>
+        )
+    }
+
+ if(jobs.length === 0)
+ {
+    return(
+        <Wrapper>
+            <h2> No Jobs to Display..</h2>
+        </Wrapper>
+    )
+ }
+
+ return(
+    <Wrapper>
+        <h5> Jobs info</h5>
+        <div className="jobs">
+            {jobs.map((job)=>{
+                return <Job key ={job._id} {...job}/>
+            })}
+        </div>
+    </Wrapper>
+ )
+  
 }
 
 export default JobsContainer
